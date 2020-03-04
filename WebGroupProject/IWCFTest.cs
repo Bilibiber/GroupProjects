c32ca202bb063d66e6625a4b50c2afc618f2c4a3
Fan Guo <guofx@dukes.jmu.edu>
2020-03-04 13:52:47 -0500

    1

---
+++ WebGroupProject/IWCFTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WebGroupProject.Models;
+
+namespace WebGroupProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWCFTest" in both code and config file together.
+    [ServiceContract]
+    public interface IWCFTest
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        List<JasonTest> getSubCategory(int CategoryID);
+    }
+}
